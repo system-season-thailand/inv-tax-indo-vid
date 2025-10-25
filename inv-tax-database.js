d3d852c9fb4bbfef0fbfad85df0dee24925b3304
Bandar-Zuhair <my.github.websites@gmail.com>
2025-10-25 23:28:45 +0700

    .

--- inv-tax-database.js
+++ inv-tax-database.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://bdisyvjhbipknpxvyctb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkaXN5dmpoYmlwa25weHZ5Y3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MTIyMzksImV4cCI6MjA2MjI4ODIzOX0.x3aLzQPaaMIUo4MDyPSeCPnG33LVEhtFhGvhY3SkdrQ';
+const supabaseUrl = 'https://zrunsrimyijarswjfycw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpydW5zcmlteWlqYXJzd2pmeWN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MjgzOTEsImV4cCI6MjA2MjMwNDM5MX0.UdW4LiIY-t1jZlrat1VUGnW0yRE7YEzW5SHbpkE29H8';
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 let new_or_imported_inv_company_variable = 'new_invoice_company';
